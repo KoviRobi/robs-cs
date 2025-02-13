218753188af8f0b23e0a8446487d5f6bdd382fbd
Kovacsics Robert <kovirobi@gmail.com>
2025-02-13 20:52:36 +0000

    Have tests a hints based on feedback

--- test/hint_1_test_take_drop_reversible.ml
+++ test/hint_1_test_take_drop_reversible.ml
@@ -1,13 +1,13 @@
 open Mergesort
 
 let test_mergesort () =
-  let n = Random.int_in_range ~min:10 ~max:100 in
-  let l = List.init n (fun _ -> Random.int 1000) in
-  for m = 0 to n do
+  let len = List.length in
+  let l = [ 1; 2; 3; 4; 5 ] in
+  for m = 0 to len l do
     let lh = take (m, l) in
     let rh = drop (m, l) in
     assert (List.length lh = m);
-    assert (List.length rh = n - m);
+    assert (List.length rh = len l - m);
     assert (lh @ rh = l)
   done
 
---
+++ test/hint_2_test_take_drop_simple.ml
@@ -0,0 +1,12 @@
+(* Allows us to refer to Mergesort.take/Mergesort.drop with just take/drop *)
+open Mergesort
+
+let _ = assert (take (0, [ 1; 2; 3 ]) = [])
+let _ = assert (take (1, [ 1; 2; 3 ]) = [ 1 ])
+let _ = assert (take (2, [ 1; 2; 3 ]) = [ 1; 2 ])
+let _ = assert (take (3, [ 1; 2; 3 ]) = [ 1; 2; 3 ])
+
+let _ = assert (drop (0, [ 1; 2; 3 ]) = [ 1; 2; 3 ])
+let _ = assert (drop (1, [ 1; 2; 3 ]) = [ 2; 3 ])
+let _ = assert (drop (2, [ 1; 2; 3 ]) = [ 3 ])
+let _ = assert (drop (3, [ 1; 2; 3 ]) = [])
